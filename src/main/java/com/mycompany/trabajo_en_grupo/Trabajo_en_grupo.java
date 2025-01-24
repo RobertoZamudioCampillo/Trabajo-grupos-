71d5fe8a8947680b571b55fd7a7d23f948a8447a
Enrique <eamugue1709@g.educaand.es>
2025-01-24 16:39:37 +0100

    Proyecto Refactorizado.

---
+++ src/main/java/com/mycompany/trabajo_en_grupo/Trabajo_en_grupo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo_en_grupo;
+
+/**
+ *
+ * @author enriq
+ */
+public class Trabajo_en_grupo {
+
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Enrique Amuedo Guerrero y estoy haciendo este trabajo con mis compañeros: Roberto Zamudio Campillo y Alfonso Roldán Vallejo");
+        System.out.println("Anthony viene al Betis por 2 millones");
+    }
+}
